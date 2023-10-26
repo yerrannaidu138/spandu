48aadc0c1904b02046806c7056ea037eaf085c82
yerrannaidu138 <148609018+yerrannaidu138@users.noreply.github.com>
2023-10-26 22:04:01 +0530

    Add files via upload

---
+++ bsy.js
@@ -0,0 +1,60 @@
+function change1()
+{
+    document.getElementById("dental").style.animation="ani2 0.5s forwards"
+    document.getElementById("con52121").style.animation="ani3 0.9s forwards";
+}
+function noChange1()
+{
+    document.getElementById("dental").style.animation="none"
+    document.getElementById("con52121").style.animation="none";
+}
+function change2()
+{
+    document.getElementById("nuero").style.animation="ani2 0.5s forwards"
+    document.getElementById("con52221").style.animation="ani3 0.9s forwards";
+}
+function noChange2()
+{
+    document.getElementById("nuero").style.animation="none"
+    document.getElementById("con52221").style.animation="none";
+}
+function change3()
+{
+    document.getElementById("cardio").style.animation="ani2 0.5s forwards"
+    document.getElementById("con52321").style.animation="ani3 0.9s forwards";
+}
+function noChange3()
+{
+    document.getElementById("cardio").style.animation="none"
+    document.getElementById("con52321").style.animation="none";
+}
+function change4()
+{
+    document.getElementById("orthopedy").style.animation="ani2 0.5s forwards"
+    document.getElementById("con52421").style.animation="ani3 0.9s forwards";
+}
+function noChange4()
+{
+    document.getElementById("orthopedy").style.animation="none"
+    document.getElementById("con52421").style.animation="none";
+}
+function change5()
+{
+    document.getElementById("lab").style.animation="ani2 0.5s forwards"
+    document.getElementById("con52521").style.animation="ani3 0.9s forwards";
+}
+function noChange5()
+{
+    document.getElementById("lab").style.animation="none"
+    document.getElementById("con52521").style.animation="none";
+}
+function change6()
+{
+    document.getElementById("psyciatry").style.animation="ani2 0.5s forwards"
+    document.getElementById("con52621").style.animation="ani3 0.9s forwards";
+}
+function noChange6()
+{
+    document.getElementById("psyciatry").style.animation="none"
+    document.getElementById("con52621").style.animation="none";
+}
